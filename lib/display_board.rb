bc39a7a08135b3ac7ec599c860e460fd56867818
Victoria Alee <victoriaallee@gmail.com>
2020-11-16 02:02:35 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -13,10 +13,10 @@ def display_board(board)
 end
 
 
-# def display_board(board)
-#   puts " #{board[0]}  |  #{board[1]} |  #{board[2] "
-#   puts "-----------"
-#   puts " #{board[3]}  |  #{board[4]} |  #{board[5] "
-#   puts "-----------"
-#   puts " #{board[6]}  |  #{board[7]} |  #{board[8] "
-# end
+def display_board(board)
+  puts " #{board[0]}  |  #{board[1]} |  #{board[2] "
+  puts "-----------"
+  puts " #{board[3]}  |  #{board[4]} |  #{board[5] "
+  puts "-----------"
+  puts " #{board[6]}  |  #{board[7]} |  #{board[8] "
+end
