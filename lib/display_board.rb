9e86338879b02bb5db793e035e65a43a79d8accc
Victoria Alee <victoriaallee@gmail.com>
2020-11-16 01:59:53 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,8 +2,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
+
 def display_board(board)
-  puts "   |   |   "
+  puts " #{board[0]}  |  #{board[1]} |  #{board[2]} "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
@@ -11,14 +12,6 @@ def display_board(board)
   puts "-----------"
 end
 
-def display_board(board)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   | X |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-end
 
 # def display_board(board)
 #   puts " #{board[0]}  |  #{board[1]} |  #{board[2] "
