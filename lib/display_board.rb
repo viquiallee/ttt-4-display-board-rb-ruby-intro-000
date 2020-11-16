154a8e5b9579c6b19402a1ed2a5324e654472d27
Victoria Alee <victoriaallee@gmail.com>
2020-11-16 01:44:10 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,14 +2,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board(board)
-    puts "   |   |   "
-    puts "-----------"
-    puts "   |   |   "
-    puts "-----------"
-    puts "   |   |   "
-end
-
 
 
 # def display_board(board)
